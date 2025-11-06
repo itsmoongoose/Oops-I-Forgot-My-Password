4eadfbbd0b305a8b031eb1f18a4c18f05bb5baa3
Lewis Lemay <lewishlemay@gmail.com>
2025-11-05 19:13:43 -0700

    Make page interactive by adding circles and checking which characters are correct

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,9 +2,44 @@
 async function main() {
     
     //Defining variables
-    const passGuess = document.getElementById("password");
+    let passGuess = document.getElementById("password");
     const answerContainer = document.getElementById("answer-container");
-    const dummyPass = "Password123";
+    const enterButton = document.getElementById("enter-button");
+    const passAnswer = "Password123";
 
-    
+    passGuess.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+
+            //Remove any whitespace
+            let passGuessTrim = passGuess.value.replace(/\s/g, "");
+
+                //Check if input matches answer
+                for (let letter = 0; letter < passGuessTrim.length; letter = letter + 1) {
+                    const circle = document.createElement("div");
+                    circle.classList.add("circle");
+                    if (passGuessTrim[letter] === passAnswer[letter]) {
+                        circle.classList.add("green-check");
+                    } else if (passAnswer.includes(passGuessTrim[letter])) {
+                        circle.classList.add("yellow-q");
+                    } else {
+                        circle.classList.add("red-x");
+                    }
+
+                    //Add circles to container
+                    answerContainer.appendChild(circle);
+
+                    //Clear input field
+                    passGuess.value = "";
+                }
+
+
+
+            event.preventDefault();
+        }
+
+    //addEventListener end
+    })
+
+//main function end    
 }
+main();
